84130777274076723233be27aab619f5310457e3
SHREESHA SHARMA U <shreeshauppangala@gmail.com>
2021-09-23 13:31:01 +0530

    first commit

---
+++ server/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  twilio: {
+    accountSid: 'AC68a0e59af0a1a4e919b13976278c4a42',
+    apiKey: 'SKe3d81cc413dbe8684c2b42019719bc45',
+    apiSecret: '9d4qQBsJzEZE3HYsam2EClsKUw3Dvmq0'
+  }
+};
